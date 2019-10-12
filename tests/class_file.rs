bd1c3b781d4f03b33922fd4c0f50d9500542148e
romangraef <romangraef@gmail.com>
2019-10-12 20:42:51 +0200

    Fixes incorrect Version Comparison
    Now ClassFileVersion.supports returns the correct result for versions with lower major version but higher minor version

--- tests/class_file.rs
+++ tests/class_file.rs
@@ -1,11 +1,17 @@
 use crate::class_file::ClassFileVersion;
+use rlass::class_file::ClassFileVersion;
 
 #[test]
 fn version_supports_test() {
-    let first = ClassFileVersion::latest();
-    let second = ClassFileVersion::new(52, 0);
+    let first = ClassFileVersion::new(53, 0);
+    let second = ClassFileVersion::new(52, 1);
+    let third = ClassFileVersion::new(52, 0);
 
     assert!(first.supports(second));
+    assert!(first.supports(third));
+    assert!(second.supports(third));
     assert_ne!(second.supports(first));
     assert!(first.supports(first));
+    assert_ne!(third.supports(second));
+    assert_ne!(third.supports(first));
 }
