73653d66e2085cec90390b3a5256e1c9f0e2b0f3
Justus K <justus.k@protonmail.com>
2019-10-14 11:54:07 +0200

    Add enum to represent a ReferenceKind

--- src/parser/constant_pool.rs
+++ src/parser/constant_pool.rs
@@ -1,8 +1,7 @@
 /// Representation of all constant pool
 /// [tags](https://docs.oracle.com/javase/specs/jvms/se11/html/jvms-4.html#jvms-4.4-140).
 #[derive(Debug, PartialEq)]
-pub enum Tag {
-    Class,
+pub enum Tag { Class,
     FieldRef,
     MethodRef,
     InterfaceMethodRef,
@@ -46,6 +45,40 @@ impl From<u8> for Tag {
             19 => Tag::Module,
             20 => Tag::Package,
             _ => Tag::Invalid,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ReferenceKind {
+    GetField,
+    GetStatic,
+    PutField,
+    PutStatic,
+    InvokeVirtual,
+    InvokeStatic,
+    InvokeSpecial,
+    NewInvokeSpecial,
+    InvokeInterface,
+    // Invalid tag is required to be returned in
+    // the From<u8> implementation if the given tag is
+    // invalid.
+    Invalid,
+}
+
+impl From<u8> for ReferenceKind {
+    fn from(kind: u8) -> Self {
+        match kind {
+            1 => ReferenceKind::GetField,
+            2 => ReferenceKind::GetStatic,
+            3 => ReferenceKind::PutField,
+            4 => ReferenceKind::PutStatic,
+            5 => ReferenceKind::InvokeVirtual,
+            6 => ReferenceKind::InvokeStatic,
+            7 => ReferenceKind::InvokeSpecial,
+            8 => ReferenceKind::NewInvokeSpecial,
+            9 => ReferenceKind::InvokeInterface,
+            _ => ReferenceKind::Invalid,
         }
     }
 }
